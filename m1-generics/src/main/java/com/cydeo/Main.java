420d91a342dae32cb6afb799f494059aa835a789
ekin <ekin.alpaslan@yandex.com>
2023-05-22 16:05:25 +0300

    up to java 5

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -1,9 +1,29 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList items = new ArrayList();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+ //     items.add("Apple");
+
+
+        printDoubled(items);
 
     }
+
+    private static void printDoubled(ArrayList items){
+        for (Object i : items){
+            System.out.println((Integer)i*2);
+        }
+    }
+
 }
