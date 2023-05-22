6d2b1138ff8e3d92a3f956137b738d648740d9d1
ekin <ekin.alpaslan@yandex.com>
2023-05-22 21:18:53 +0300

    Enum

--- m2-enum/src/main/java/Currency.java
+++ m2-enum/src/main/java/Currency.java
@@ -1,7 +1,16 @@
 public enum Currency {
 
-    PENNY, NICKEL, DIME, QUARTER
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
 
     //Penny is an object on currency class
 
+
+    private int value;
+    Currency(int value) {
+        this.value=value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
---
+++ m2-enum/src/main/java/ProjectStatus.java
@@ -0,0 +1,14 @@
+public enum ProjectStatus {
+
+    OPEN,CLOSED,PROGRESS("IN PROGRESS");
+
+    String status;
+
+
+    ProjectStatus(String status) {
+        this.status = status;
+    }
+
+    ProjectStatus() {
+    }
+}
