33a9de7135f231d10b41097dbb70ec7df30ca148
ekin <ekin.alpaslan@yandex.com>
2023-05-24 12:34:57 +0300

    Lambda

---
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -0,0 +1,36 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<Apple>();
+        inventory.add(new Apple(300,Color.GREEN));
+        inventory.add(new Apple(100,Color.RED));
+        inventory.add(new Apple(200,Color.GREEN));
+        inventory.add(new Apple(50,Color.RED));
+
+
+        List<Apple> heavyApple = filterApples(inventory,new AppleWeightPredicate());
+        System.out.println(heavyApple);
+        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+
+
+
+    }
+    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate){
+
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory){
+            if(predicate.test(apple)){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+}
