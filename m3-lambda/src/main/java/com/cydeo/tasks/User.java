d13e097551b3e564e5d0232d4e84d8b88c9be4aa
ekin <ekin.alpaslan@yandex.com>
2023-05-25 22:06:59 +0300

    Lambda task 3

---
+++ m3-lambda/src/main/java/com/cydeo/tasks/User.java
@@ -0,0 +1,15 @@
+package com.cydeo.tasks;
+
+import lombok.*;
+
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+@Data
+@AllArgsConstructor
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+}
