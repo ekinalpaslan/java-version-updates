f2fca97686373fa0ee28cc3925c18d927c821975
ekin <ekin.alpaslan@yandex.com>
2023-05-26 22:56:06 +0300

    Double Colon Operator

---
+++ m4-doble_colon_operators/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculate sum = (x,y) -> System.out.println(x+y);
+
+        Calculate s1 = (x,y) -> Calculator.findSum(x,y);
+
+        //Reference to a Static Method
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10,20);
+
+        //Reference to an Instance Method
+        Calculator obj = new Calculator();
+        Calculate s3 =  (x,y) -> obj.findMultiply(x,y);
+        Calculate s4 = obj::findMultiply;
+
+        //Reference to a Functional Interface in Java
+        BiFunction<String,Integer,String> func = (str,i) -> str.substring(i);
+        BiFunction<String,Integer,String> f1 = String::substring;   //obj is not created cause return type is same with non-static class
+
+        //Reference to a Functional Interface accepting also the class name as a parameter
+        Function<Integer,Double> b = new MyClass()::method;
+        BiFunction<MyClass,Integer,Double> b1 = MyClass::method;    //you can just write class name without new obj
+        b1.apply(new MyClass(),5);
+
+
+
+    }
+}
